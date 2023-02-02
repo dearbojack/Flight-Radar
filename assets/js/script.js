fb5a1106532acfb6ef56ff1b81e7ee192309de93
Dali Han <ross4han@gmail.com>
2023-02-02 07:53:36 +0000

    delete playtest code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,1 @@
-fetch('https://restcountries.com/v2/all')
-  .then(response => response.json())
-  .then(countries => {
-    const countrySelect = document.getElementById('country-select');
 
-    countries.forEach(country => {
-      const option = document.createElement('option');
-      option.value = country.name;
-      option.text = country.name;
-      option.style.backgroundImage = `url(${country.flag})`;
-      countrySelect.add(option);
-    });
-  });
