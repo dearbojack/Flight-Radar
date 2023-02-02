9b71dda6fb5d70dde4ae1977f6c3d494415b6725
Dali Han <ross4han@gmail.com>
2023-02-02 08:18:45 +0000

    for loop to create newCard

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
 const apiKey = "YfIUBElKGXDPyEkqeoTHKUNqWudUQyeC";
 
 // * keywords : country names from user selection
-var countrySelect = "";
+var countrySelect = "america";
 
 // * date : current year
 
@@ -24,4 +24,28 @@ $.ajax({
 }).then( function(r) {
     console.log(queryUrl);
     console.log(r);
+    // get the array of all news returned
+    let docArray = r.response.docs;
+    
+    for (let i = 0; i < docArray.length; i++) {
+      // get headline and link it
+      let headLine = docArray[i].headline.main;
+      let webUrl = docArray[i].web_url;
+      let headLineWLink = $("<h1>").html(`<a href="${webUrl}">${headLine}</a>`);
+      // get author
+      let byline = $("<p>").text(docArray[i].byline.original);
+      // get word count
+      let wordCount = $("<p>").text(docArray[i].word_count);
+      // get pub date
+      let pubDate = $("<p>").text(docArray[i].pub_date);
+      // get snippet
+      let snippet = $("<p>").text(docArray[i].snippet);
+
+      // create news card
+      let newsCardDiv = $("<div>").addClass("news-card");
+
+      newsCardDiv.append(headLineWLink, pubDate, wordCount, byline, webUrl, snippet);
+      $("body").append(newsCardDiv);
+      
+    }
 });
