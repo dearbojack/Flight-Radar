a6099ef209586e7568f3648457929ecb186709b7
shah_hoque1 <shah.hoque10@gmail.com>
2023-02-06 22:14:39 +0000

    header resized

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,10 @@ $(window).on('resize', function() {
     $('.top-right-btn').css('font-size', 'x-large');
     $('.btn-primary').css('padding-top', '0px');
     $('.btn-primary').css('padding-bottom', '0.2em');
-    $('.btn-primary').css('text-align', 'center');
 } else {
     $('.top-right-btn').html('Home Location');
     $('.top-right-btn').css('font-size', '');
+    $('.btn-primary').css('padding-top', '');
+    $('.btn-primary').css('padding-bottom', '');
   }
 });
