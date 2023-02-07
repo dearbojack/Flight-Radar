198de8499be8405c1a4d66ca125ce27909aa4158
shah_hoque1 <shah.hoque10@gmail.com>
2023-02-07 12:46:55 +0000

    stats for tablet view updated

--- assets/js/country.js
+++ assets/js/country.js
@@ -100,51 +100,30 @@ function getCard() {
     curSymValue.css("font-weight", "400").text(currencysym)
     curSym.append(curSymValue)
 
-    country.append(divTitle)
-    country.append(header);
-    country.append(cFlag);
-    country.append(cap);
-    country.append(population);
-    country.append(language);
-    country.append(dem);
-    country.append(reg);
-    country.append(subReg);
-    country.append(cash);
-    country.append(curSym);
+    
+       // create two divs
 
-    // populate for the media query (tablet size)
-    var countryTabDiv1 = $("#country-div-2-img");
-    var countryTabDiv2 = $("#country-div-2-text");
+       var countryHeader = $('<div>').attr('id', 'country-header');
+       var countryStats = $('<div>').attr('id', 'country-stats');
+   
+   
+   
+       countryHeader.append(header);
+       countryHeader.append(cFlag);
+       countryStats.append(cap);
+       countryStats.append(population);
+       countryStats.append(language);
+       countryStats.append(dem);
+       countryStats.append(reg);
+       countryStats.append(subReg);
+       countryStats.append(cash);
+       countryStats.append(curSym);
+       country.append(countryHeader, countryStats)
+     });
 
-  //  clones where needed as you can't append an element twice
-    var divTitleClone = divTitle.clone();
-    var headerClone = header.clone();
-    var cFlagClone = cFlag.clone();
-    var capClone = cap.clone();
-    var populationClone = population.clone();
-    var languageClone = language.clone();
-    var demClone = dem.clone();
-    var regClone = reg.clone();
-    var subRegClone = subReg.clone();
-    var cashClone = cash.clone();
-    var curSymClone = curSym.clone();
-
-
-    countryTabDiv1.append(headerClone);
-    countryTabDiv1.append(cFlagClone);
-
-    countryTabDiv2.append(capClone)
-    countryTabDiv2.append(populationClone)
-    countryTabDiv2.append(languageClone)
-    countryTabDiv2.append(demClone)
-    countryTabDiv2.append(regClone)
-    countryTabDiv2.append(subRegClone)
-    countryTabDiv2.append(cashClone)
-    countryTabDiv2.append(curSymClone)
-
-  });
+ 
 
   getCapital(searchedCountry).then(capital => {
     renderTodayWeather(capital);
 });
-  }
+}
