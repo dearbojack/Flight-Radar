e2c74cda45b2174881956cf485f5a4eec240955c
shah_hoque1 <shah.hoque10@gmail.com>
2023-02-07 19:36:05 +0000

    event l updated for home location

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
 // Change home location button (media query)
-var homeBtn = window.matchMedia("(max-width: 1000px)");
+$(document).ready(function() {
+  var homeBtn = window.matchMedia("(max-width: 1000px)");
 
-$(window).on('resize', function() {
+  // if on load ((document).ready) matches homeBtn, then ...
   if (homeBtn.matches) {
     $('.top-right-btn').html('⌂');
     $('.top-right-btn').css('font-size', 'x-large');
     $('.btn-primary').css('padding-top', '0px');
     $('.btn-primary').css('padding-bottom', '0.2em');
-} else {
-    $('.top-right-btn').html('Home Location');
-    $('.top-right-btn').css('font-size', '');
-    $('.btn-primary').css('padding-top', '');
-    $('.btn-primary').css('padding-bottom', '');
   }
+
+  // listen for resizing
+  $(window).on('resize', function() {
+    if (homeBtn.matches) {
+      $('.top-right-btn').html('⌂');
+      $('.top-right-btn').css('font-size', 'x-large');
+      $('.btn-primary').css('padding-top', '0px');
+      $('.btn-primary').css('padding-bottom', '0.2em');
+    } else {
+      $('.top-right-btn').html('Home Location');
+      $('.top-right-btn').css('font-size', '');
+      $('.btn-primary').css('padding-top', '');
+      $('.btn-primary').css('padding-bottom', '');
+    }
+  });
 });
